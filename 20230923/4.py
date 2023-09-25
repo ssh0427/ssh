a6532cc954cda6f4f740875a8eb2055d80358acb
ssh <cherub0427@gmail.com>
2023-09-25 20:44:46 +0900

    Mastered star coding

--- 20230923/4.py
+++ 20230923/4.py
@@ -12,3 +12,44 @@ for i in range(0,10):
     for j in range(0,i):
         print("*", end="a")
     print()
+
+
+
+for i in range(0,10):
+    for k in range(0,i):
+        print(" ", end="")
+    for j in range(0,10-i):
+        print("*", end="")
+    print()
+
+
+
+for i in range(0,10):
+    for k in range(0,10-i):
+        print(" ", end="")
+    for j in range(0,i):
+        print("*", end="")
+    print()
+
+
+
+for i in range(0,10):
+    for j in range(0,i):
+        print("*", end="")
+    for k in range(0,10-i):
+        print("#", end="")
+    print()
+
+
+
+for i in range(1,11):
+    for j in range(0,10-i):
+        print(" ", end="")
+    for k in range(0,2*i-1):
+        print("*", end="")
+    for m in range(0,10-i):
+        print(" ", end="")
+    print()
+
+
+
